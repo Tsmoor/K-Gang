f2d615cd78f36299cb6a4e1ba772a05a29c013b8
Tsmoor <59980509+Tsmoor@users.noreply.github.com>
2025-07-20 17:32:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-const CACHE = "kga-v1";
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/","/index.html","/css/style.css","/js/app.js","/manifest.json"]))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE = "kga-v3";
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll([
+        "/",
+        "/index.html",
+        "/admin.html",
+        "/css/style.css",
+        "/css/dark.css",
+        "/js/app.js",
+        "/js/games.js",
+        "/js/store.js",
+        "/js/admin.js",
+        "/js/utils.js",
+        "/manifest.json"
+      ])
+    )
+  );
+});
+self.addEventListener("fetch", e =>
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
+);
